1c5b8e28402cefe33d67ddc637cb81a86a78582e
Sebas <sebas.654@gmail.com>
2022-04-13 03:22:10 -0300

    Creacion Proyecto

---
+++ proyecto1010/src/main/java/ar/edu/unju/fi/proyecto1010/Proyecto1010Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.proyecto1010;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto1010Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto1010Application.class, args);
+	}
+
+}
---
+++ proyecto1010/src/test/java/ar/edu/unju/fi/proyecto1010/Proyecto1010ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.proyecto1010;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto1010ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
